0f01c8f5bae3afec93d510c947835743f981927f
adrianagm <adriana-4@hotmail.com>
2014-09-04 11:33:17 +0200

    Transformación de coordenadas

--- geoservices-routing-graphhopper/src/main/java/com/emergya/geoservices/routing/graphhopper/GHRouterImpl.java
+++ geoservices-routing-graphhopper/src/main/java/com/emergya/geoservices/routing/graphhopper/GHRouterImpl.java
@@ -4,6 +4,8 @@ import com.emergya.geoservices.routing.utils.PointUtils;
 import com.emergya.geoservices.routing.wsdl.Edge;
 import com.emergya.geoservices.routing.wsdl.FindShortestPath;
 import com.emergya.geoservices.routing.wsdl.FindShortestPathResponse;
+import com.emergya.geoservices.routing.wsdl.FindShortestPathResponse.Messages;
+import com.emergya.geoservices.routing.wsdl.Message;
 import com.emergya.geoservices.routing.wsdl.Point;
 import com.emergya.geoservices.routing.wsdl.Step;
 import com.graphhopper.GHRequest;
@@ -12,8 +14,23 @@ import com.graphhopper.GraphHopperAPI;
 import com.graphhopper.http.GraphHopperWeb;
 import com.graphhopper.util.Instruction;
 import com.graphhopper.util.shapes.GHPlace;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.CoordinateSequence;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.geotools.referencing.CRS;
+import org.geotools.geometry.jts.JTS;
+import org.opengis.geometry.MismatchedDimensionException;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -31,69 +48,74 @@ public class GHRouterImpl implements GHRouter {
 
     @Override
     public FindShortestPathResponse createRoute(FindShortestPath parameters) {
-       
-        if(gh == null) {
+
+        if (gh == null) {
             gh = new GraphHopperWeb();
             gh.load(GRAPHHOPPER_URL);
         }
 
-        GHResponse route = gh.route(createGHRequest(parameters));
-
+        GHResponse route = null;
         FindShortestPathResponse response = new FindShortestPathResponse();
-        FindShortestPathResponse.Path path = new FindShortestPathResponse.Path();        
+        try {
+            route = gh.route(createGHRequest(parameters));
+        } catch (FactoryException | TransformException ex) {
+            Message message = new Message();
+            message.setText("Transform coordinates has failed");
+            Messages messages = new FindShortestPathResponse.Messages();
+            response.setMessages(messages);
+            Logger.getLogger(GHRouterImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        FindShortestPathResponse.Path path = new FindShortestPathResponse.Path();
         response.setPath(path);
-        
+
         List<Step> steps = path.getStep();
-        
-        
+
         Iterator<Instruction> instructions = route.getInstructions().iterator();
-        
-        int i =0;
-        
+
+        int i = 0;
+
         double accTime = 0.0;
         double accDistance = 0.0;
-        
-        while(instructions.hasNext()) {
+
+        while (instructions.hasNext()) {
             Instruction instruction = instructions.next();
-                    
-            
+
             Step step = new Step();
             step.setOrder(i);
-            
+
             String instructionText = instruction.getName();
             // We need to clean it a bit:
             int ontoIndex = instructionText.indexOf(_ONTO_);
-            if(ontoIndex>=0) {
-                instructionText = instructionText.substring(ontoIndex+ _ONTO_.length());
+            if (ontoIndex >= 0) {
+                instructionText = instructionText.substring(ontoIndex + _ONTO_.length());
             }
-            
-            if(instructionText.equals("Continue")) {
+
+            if (instructionText.equals("Continue")) {
                 instructionText = "Unknown";
             }
-            
+
             step.setName(instructionText);
             step.setType(7);
-                    
+
             accDistance += instruction.getDistance();
             step.setDistance(accDistance);
-            
-            accTime+= instruction.getTime()/1000;            
+
+            accTime += instruction.getTime() / 1000;
             step.setTime(accTime);
-            
+
             List<Edge> edges = step.getEdge();
-            
-            
-            
+
             steps.add(step);
-            
+
             i++;
         }
-        
+
         return response;
     }
     private static final String _ONTO_ = " onto ";
 
-    private GHRequest createGHRequest(FindShortestPath parameters) {
+    private GHRequest createGHRequest(FindShortestPath parameters) throws FactoryException, TransformException {
         GHRequest request = new GHRequest(
                 toGHPlace(parameters.getStartPoint()),
                 toGHPlace(parameters.getTargetPoint()));
@@ -101,9 +123,30 @@ public class GHRouterImpl implements GHRouter {
         return request;
     }
 
-    private GHPlace toGHPlace(Point p) {
+    private GHPlace toGHPlace(Point p) throws FactoryException, MismatchedDimensionException, TransformException {
         double[] coordinates = PointUtils.toCoordinates(p);
-        GHPlace gHPlace = new GHPlace(coordinates[0], coordinates[1]);
+
+        Coordinate c = new Coordinate(coordinates[0], coordinates[1]);
+        Coordinate[] coord = new Coordinate[1];
+        coord[0] = c;
+        CoordinateSequence coorSeq = new CoordinateArraySequence(coord);
+
+        GeometryFactory geometryFactory = new GeometryFactory();
+        geometryFactory.createPoint(coorSeq);
+
+        Geometry geometry = new com.vividsolutions.jts.geom.Point(coorSeq, geometryFactory);
+
+        String srs = p.getSrsName();
+        CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:" + srs);
+        CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:4326");
+        MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS);
+        Geometry targetGeometry = JTS.transform(geometry, transform);
+        
+        
+        com.vividsolutions.jts.geom.Point point = targetGeometry.getCentroid();
+        System.out.println("EPSG inicial " + srs +':' + p.getCoordinates());
+        System.out.println("EPSG final 4326:" + point);
+        GHPlace gHPlace = new GHPlace(point.getX(), point.getY());
 
         return gHPlace;
     }
